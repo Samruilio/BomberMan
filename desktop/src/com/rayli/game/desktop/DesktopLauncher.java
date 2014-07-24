268d14381bd6bcdd27da6d1a0fa03c8475fb8788
Samruilio <samruilio@gmail.com>
2014-07-25 00:14:25 +1000

    First commit

---
+++ desktop/src/com/rayli/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rayli.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rayli.game.BomberMan;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BomberMan(), config);
+	}
+}
